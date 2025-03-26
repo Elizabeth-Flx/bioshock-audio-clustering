0b3b96122942c56ebede892c8bee150c1dc2bb30
Elizabeth Lochert <daniel.lochert.flx@gmail.com>
2025-03-26 20:09:04 +0100

    Add: speechbrain can now be loaded

--- generate_embedding.py
+++ generate_embedding.py
@@ -1,9 +1,37 @@
 import torchaudio
+import os
+# os.environ["SB_DISABLE_SYMLINKS"] = "1"  # Completely disable symlinks
+# os.environ["SB_DOWNLOAD_STRATEGY"] = "COPY"
+
+from speechbrain.pretrained import EncoderClassifier
+from speechbrain.utils.fetching import LocalStrategy
+
+wav_path = r"F:\Workspace\bioshock-audio-clustering\all_voicelines_wav"
+emb_path = r"F:\Workspace\bioshock-audio-clustering\all_voicelines_emb"
+
+# Create output folder if it doesn't exist
+os.makedirs(emb_path, exist_ok=True)
 
 
+# Load the model
+classifier = EncoderClassifier.from_hparams(
+    source="speechbrain/spkrec-ecapa-voxceleb",
+    savedir="tmpdir_spkrec-ecapa-voxceleb",
+    # use_auth_token=False,
+    # local_files_only=False,
+    # local_strategy=LocalStrategy.COPY  # Use COPY instead of SYMLINK)
+)
 
-audio_path = "./all_voicelines_wav/"
+# Embed wav file
+def generate_embedding(wav_file):
+    signal, sr = torchaudio.load(wav_file)
+    embedding = classifier.encode_batch(signal)
+    return embedding
 
-signal, sr = torchaudio.load(audio_path)
+ 
+wav_files = [os.path.join(wav_path, file) for file in os.listdir(wav_path)]
+print(wav_files[0]) 
 
-print(signal)
+
+print(generate_embedding(wav_files[0]))
+
